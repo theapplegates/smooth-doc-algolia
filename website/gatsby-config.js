32dbe4020196c7e16e0040ead6650a6890a1ac88
Paul Applegate <paul@theapplegates.com>
2021-10-30 18:00:52 -0400

    first commit

---
+++ website/gatsby-config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        withAvif: true,
+        withWebp: true,
+        maxWidth: 630,
+      },
+    },
+
+    {
+      resolve: 'smooth-doc',
+      options: {
+        name: 'Smooth DOC',
+        siteUrl: 'https://smooth-doc.com',
+        description: 'Ready to use documentation theme for Gatsby.',
+        baseDirectory: path.resolve(__dirname, '../'),
+        author: 'Greg Bergé',
+        sections: ['About', 'Guide', 'Components', 'Reference'],
+        navItems: [{ title: 'Docs', url: '/docs/' }],
+        twitterAccount: 'neoziro',
+        githubRepositoryURL: 'https://github.com/gregberge/smooth-doc/',
+        carbonAdsURL:
+          '//cdn.carbonads.com/carbon.js?serve=CE7IL2JN&placement=xstyleddev',
+        docSearch: {
+          apiKey: 'a0c23a738fe1cfffa923daba2eb185be',
+          indexName: 'smooth-doc',
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: 'UA-154496255-5',
+      },
+    },
+  ],
+}
